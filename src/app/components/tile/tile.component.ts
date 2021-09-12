d79094915fdf051d0db447da14f7a339846f2a72
Ersanio <ersan.io@outlook.com>
2021-09-13 00:57:43 +0200

    Fix HTML to make use of the new flat array

--- src/app/components/tile/tile.component.ts
+++ src/app/components/tile/tile.component.ts
@@ -1,42 +1,32 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Tile } from 'src/app/models/tile';
 
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.scss']
 })
-export class TileComponent implements OnInit {
+export class TileComponent {
   public readonly gapX: number = 13;
   public readonly gapY: number = 14;
   public readonly heightX: number = 90;
   public readonly heightY: number = 90;
 
   @Input()
-  public posX: number = 0;
-
-  @Input()
-  public posY: number = 0;
-
-  @Input()
-  public value: number = 0;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public tile: Tile;
 
   public getStyle(): any {
     return {
       'transform': 'translate(' + this.getTileX() + 'px, ' + this.getTileY() + 'px)',
-      'background-image': 'url(assets/' + this.value +'.png)'
+      'background-image': 'url(assets/' + this.tile.value +'.png)'
      }
   }
 
   public getTileX(): number {
-    return (this.posX * this.heightX) + (this.posX * this.gapX);
+    return (this.tile.position.x * this.heightX) + (this.tile.position.x * this.gapX);
   }
 
   public getTileY(): number {
-    return (this.posY * this.heightY) + (this.posY * this.gapY);
+    return (this.tile.position.y * this.heightY) + (this.tile.position.y * this.gapY);
   }
 }
