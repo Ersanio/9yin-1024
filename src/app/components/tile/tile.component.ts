e59336a35f3f110eeb5ffb1247248db3dc14228e
Ersanio <ersan.io@outlook.com>
2021-10-01 22:48:50 +0200

    Add less lazy method of scaling down the game

--- src/app/components/tile/tile.component.ts
+++ src/app/components/tile/tile.component.ts
@@ -6,27 +6,36 @@ import { Tile } from 'src/app/models/tile';
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.scss']
 })
-export class TileComponent {
+export class TileComponent implements OnInit {
+
   public readonly gapX: number = 13;
   public readonly gapY: number = 14;
   public readonly heightX: number = 90;
   public readonly heightY: number = 90;
 
+  private gameScale: number;
+  private tileSize: number;
+
   @Input()
   public tile: Tile;
+
+  public ngOnInit(): void {
+    this.gameScale = Number.parseFloat(getComputedStyle(document.querySelector(':root') as Element).getPropertyValue('--scale-game'));
+    this.tileSize = Number.parseInt(getComputedStyle(document.querySelector(':root') as Element).getPropertyValue('--tile-size'), 10);
+  }
 
   public getStyle(): any {
     return {
       transform: 'translate(' + this.getTileX() + 'px, ' + this.getTileY() + 'px)',
-      'background-position-x': `-${(Math.log2(this.tile.value)-1) * 90}px`,
+      'background-position-x': `-${(Math.log2(this.tile.value) - 1) * (this.tileSize * this.gameScale)}px`,
     };
   }
 
   public getTileX(): number {
-    return (this.tile.position.x * this.heightX) + (this.tile.position.x * this.gapX);
+    return ((this.tile.position.x * this.heightX) + (this.tile.position.x * this.gapX)) * this.gameScale;
   }
 
   public getTileY(): number {
-    return (this.tile.position.y * this.heightY) + (this.tile.position.y * this.gapY);
+    return ((this.tile.position.y * this.heightY) + (this.tile.position.y * this.gapY)) * this.gameScale;
   }
 }
